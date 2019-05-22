fd17be56ad8ad15840f65a1368db79dcc4d6b602
Elvin <elvin@athennian.com>
2019-05-22 13:28:27 -0600

    trying local emulation

--- public/request.js
+++ public/request.js
@@ -1,6 +1,11 @@
 // interface with backend
 
+const deployment = false
+
 const requestService = (url, method, params, success, error) => {
+    const serverURL = deployment? `https://us-central1-seng471project.cloudfunctions.net/app`: `http://localhost:5001/seng471project/us-central1/app`
+
+
     console.log("URL", url)
     console.log("METHOD", method)
     console.log("PARAMS", params)
@@ -23,9 +28,9 @@ const requestService = (url, method, params, success, error) => {
         }
     }
 
-    req.open(method,`https://us-central1-seng471project.cloudfunctions.net/app${url}`,true) // backend
+    req.open(method,`${serverURL}${url}`,true) // backend
     req.setRequestHeader("Content-Type", "application/json")
     req.setRequestHeader('Access-Control-Allow-Headers', '*')
-    req.setRequestHeader('Access-Control-Allow-Origin', `https://seng471project.firebaseapp.com/`) // front end
+    req.setRequestHeader('Access-Control-Allow-Origin', `*`) // front end
     req.send(JSON.stringify(params))
 }
