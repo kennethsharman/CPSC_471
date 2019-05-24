70381ead56ce9ba98c06edf268ee20f58558fceb
Peter Schulze <peterkschulze@gmail.com>
2019-05-23 20:18:43 -0600

    remove response headers from request

--- public/request.js
+++ public/request.js
@@ -3,7 +3,7 @@
 const deployment = false
 
 const requestService = (url, method, params, success, error) => {
-    const serverURL = deployment? `https://us-central1-seng471project.cloudfunctions.net/app`: `http://localhost:5001/seng471project/us-central1/app`
+    const serverURL = deployment? `https://us-central1-seng471project.cloudfunctions.net/app`: `http://localhost:5002/seng471project/us-central1/app`
 
 
     console.log("URL", url)
@@ -13,7 +13,7 @@ const requestService = (url, method, params, success, error) => {
     console.log(JSON.stringify(params))
 
     let req = new XMLHttpRequest()
-    
+
     // interfaces response from back end
     req.onreadystatechange = function(){
         if (this.readyState == 4 && this.status == 200) {
@@ -30,7 +30,5 @@ const requestService = (url, method, params, success, error) => {
 
     req.open(method,`${serverURL}${url}`,true) // backend
     req.setRequestHeader("Content-Type", "application/json")
-    req.setRequestHeader('Access-Control-Allow-Headers', '*')
-    req.setRequestHeader('Access-Control-Allow-Origin', `*`) // front end
     req.send(JSON.stringify(params))
 }
